2bb5db7dcb4ea2dc9ce3610615c9436bdb3c9793
Mitch.D <michel-dufour@neuf.fr>
2021-05-29 16:15:24 +0200

    Updates livre.php
    On transforme la page en utilisant la class Livre (POO)

--- livres.php
+++ livres.php
@@ -1,4 +1,20 @@
-<?php ob_start(); ?>
+<?php
+
+//on a besoin de la classe Livre
+require "LivreClass.php";
+
+//creation de 5 livres en utilisant la classe Livre
+//Ajout de ces livres dans la variable $livres de type tableau de Livre
+$l1 = new Livre(1, "Et que ne durent que les moments doux", "Virginie Grimaldi", "livre1.jpg");
+$l2 = new Livre(2, "Changer l'eau des fleurs", "Valérie Perrin", "livre2.jpg");
+$l3 = new Livre(3, "Le Chant d'Achille", "Madeline Miller", "livre3.jpg");
+$l4 = new Livre(4, "Jujutsu Kaisen", "Gege Akutami", "livre4.jpg");
+$l5 = new Livre(5, "Il etait deux fois", "Franck Thilliez", "livre5.jpg");
+
+//on met les 5 livres dans un tableau $livres
+$livres = [$l1, $l2, $l3, $l4, $l5];
+
+ob_start(); ?>
 
 <a href="#link" type="button" class="btn btn-info mb-4">Ajouter un livre</a>
 
@@ -13,61 +29,26 @@
         </tr>
     </thead>
     <tbody>
-        <tr class="table-light">
-            <th scope="row" class="align-middle">#1</th>
-            <td class="align-middle"><img src="public/img/livre1.jpg" width="70px"></td>
-            <td class="align-middle">Et que ne durent que les moments doux</td>
-            <td class="align-middle">Virginie Grimaldi</td>
-            <td width=250 class="align-middle"><a href="#link" type="button" class="btn btn-outline-info">Voir</a>
-                <a href="#link" type="button" class="btn btn-outline-warning">Modifier</a>
-                <a href="#link" type="button" class="btn btn-outline-danger">Supprimer</a>
-            </td>
-        </tr>
-        <tr class="table-light">
-            <th scope="row" class="align-middle">#2</th>
-            <td class="align-middle"><img src="public/img/livre2.jpg" width="70px"></td>
-            <td class="align-middle">Changer l'eau des fleurs</td>
-            <td class="align-middle">Valérie Perrin</td>
-            <td width=250 class="align-middle"><a href="#link" type="button" class="btn btn-outline-info">Voir</a>
-                <a href="#link" type="button" class="btn btn-outline-warning">Modifier</a>
-                <a href="#link" type="button" class="btn btn-outline-danger">Supprimer</a>
-            </td>
-        </tr>
-        <tr class="table-light">
-            <th scope="row" class="align-middle">#3</th>
-            <td class="align-middle"><img src="public/img/livre3.jpg" width="70px"></td>
-            <td class="align-middle">Le Chant d'Achille</td>
-            <td class="align-middle">Madeline Miller</td>
-            <td width=250 class="align-middle"><a href="#link" type="button" class="btn btn-outline-info">Voir</a>
-                <a href="#link" type="button" class="btn btn-outline-warning">Modifier</a>
-                <a href="#link" type="button" class="btn btn-outline-danger">Supprimer</a>
-            </td>
-        </tr>
-        <tr class="table-light">
-            <th scope="row" class="align-middle">#4</th>
-            <td class="align-middle"><img src="public/img/livre4.jpg" width="70px"></td>
-            <td class="align-middle">Jujutsu Kaisen</td>
-            <td class="align-middle">Gege Akutami</td>
-            <td width=250 class="align-middle"><a href="#link" type="button" class="btn btn-outline-info">Voir</a>
-                <a href="#link" type="button" class="btn btn-outline-warning">Modifier</a>
-                <a href="#link" type="button" class="btn btn-outline-danger">Supprimer</a>
-            </td>
-        </tr>
-        <tr class="table-light">
-            <th scope="row" class="align-middle">#5</th>
-            <td class="align-middle"><img src="public/img/livre5.jpg" width="70px"></td>
-            <td class="align-middle">Il etait deux fois</td>
-            <td class="align-middle">Franck Thilliez</td>
-            <td width=250 class="align-middle"><a href="#link" type="button" class="btn btn-outline-info">Voir</a>
-                <a href="#link" type="button" class="btn btn-outline-warning">Modifier</a>
-                <a href="#link" type="button" class="btn btn-outline-danger">Supprimer</a>
-            </td>
-        </tr>
+
+        <?php for($i = 0; $i < count($livres); $i++) : ?>
+
+            <tr class="table-light">
+                <th scope="row" class="align-middle">#<?= $livres[$i]->getId(); ?></th>
+                <td class="align-middle"><img src="public/img/<?= $livres[$i]->getImage(); ?>" width="70px"></td>
+                <td class="align-middle"><?= $livres[$i]->getTitre(); ?></td>
+                <td class="align-middle"><?= $livres[$i]->getAuteur(); ?></td>
+                <td width=250 class="align-middle"><a href="#link" type="button" class="btn btn-outline-info">Voir</a>
+                    <a href="#link" type="button" class="btn btn-outline-warning">Modifier</a>
+                    <a href="#link" type="button" class="btn btn-outline-danger">Supprimer</a>
+                </td>
+            </tr>
+
+        <?php endfor; ?>
+        
     </tbody>
 </table>
 
 <?php
-
 $content = ob_get_clean();
 $titre = "Les livres de la bibliotheque";
 require "template.php";
